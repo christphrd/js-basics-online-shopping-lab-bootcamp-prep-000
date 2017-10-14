9e6d44033a0be1fff08918c3c833bcfd769c6432
Christopher Diep <diep.christopher@gmail.com>
2017-10-14 00:42:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++ ) {
     var check = cart[i]
-    if (check.hasOwnProperty(item)){
+    if (check.hasOwnProperty(item)=== true){
       cart = cart.splice(i,1);
     } else {
       console.log(`That item is not in your cart.`)
