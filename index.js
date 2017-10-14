030680308acf20895402265cfeaac4480a4f208a
Christopher Diep <diep.christopher@gmail.com>
2017-10-14 00:36:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++ ) {
     var check = Object.keys(cart[i])
-    var checkagain = check.toString()
+    var checkagain = check.shift()
     if (checkagain.hasOwnProperty(item)){
       cart = cart.splice(i,1);
     } else {
