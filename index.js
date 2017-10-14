439f04eee3c0d73cfc16dff7e22b2972764104a9
Christopher Diep <diep.christopher@gmail.com>
2017-10-14 01:23:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function removeFromCart(item) {
       var newcart = cart.splice(i,1);
     }
   }
-  if (typeof newcart !== 'undefined') {
+  if (typeof newcart == undefined) {
     console.log(`That item is not in your cart.`)
   }
 }
