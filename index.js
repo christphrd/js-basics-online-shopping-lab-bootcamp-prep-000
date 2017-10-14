9d347390232e00fbf67f0bf326c0c238eea7f8d0
Christopher Diep <diep.christopher@gmail.com>
2017-10-14 00:41:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++ ) {
-    var check = cart[i]  
+    var check = cart[i]
     if (check.hasOwnProperty(item)){
       cart = cart.splice(i,1);
     } else {
